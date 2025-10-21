6fe1046881e9cd8f18520bf974f5c07877fc5861
Lan <lanngoctran92@gmail.com>
2025-10-21 21:37:31 +0700

    ID-12: Import/export2

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
-let name1 = "Test with me";
-let name2 = "Test with me";
-let name3 = `Name 1: ${name1}, Name 2: ${name2}`;
-let name4 = "Name 1: " + name1 + "Name 2: " + name2;
-console.log(name3);
-const rawData: string = " ngUYỄN VAN a;25;devELOPER ";
-console.log(rawData.trim());
-let arrayData = rawData.split(";");
-console.log(rawData.substring(1, 13));
-console.log(arrayData[2]?.toLowerCase().includes("dev"));
+// cách import export trong typescript
+// import { PIPI, Student, SuperSum } from "./importExport";
 
-console.log(arrayData[0]?.toLowerCase().replace("n", "N"));
+// console.log(PIPI);
+// console.log(SuperSum(2, 3));
 
-//Regular expression
-// https://regexr.com/
-let fruit = [" Xoai", " Coc", " Buoi", " Me"];
-let result = fruit.filter((value: string, index: number) => {
-  return value.includes("a");
-});
-console.log(result);
+// let student: Student = { name: "Tai", age: 20 };
 
-fruit.forEach((val) => {
-  console.log(val);
-});
+import AAA from "./importExport";
+console.log(AAA);
 
-let newFruit = fruit.map((value) => {
-  return value.trim();
-});
-
-console.log(fruit);
-console.log(newFruit);
+export * from "./importExport";
